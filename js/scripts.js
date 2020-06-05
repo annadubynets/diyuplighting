d58d3d5741bdf3b7e71c0bf83bccef902f1b9b3c
Anna Dubynets <arogulina@gmail.com>
2020-06-05 16:57:03 +0300

    fix datepicker

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,9 @@
-
-// ---------------------
-// Date picker
-// ---------------------
-
-function updateDatepickerUi(selector, date) {
-    var today = date || new Date();
-    var prev = new Date(today);
-    prev.setMonth(today.getMonth() - 1);
-
-    var next = new Date(today);
-    next.setMonth(next.getMonth() + 1);
-
-    console.log(
-        $(selector).datepicker.DPGlobal.formatDate(prev, "MM", "en"),
-        $(selector).datepicker.DPGlobal.formatDate(today, "MM", "en"),
-        $(selector).datepicker.DPGlobal.formatDate(next, "MM", "en")
-    );
-
-    $(".datepicker-days .prev").text($(selector).datepicker.DPGlobal.formatDate(prev, "MM", "en"));
-    $(".datepicker-days .next").text($(selector).datepicker.DPGlobal.formatDate(next, "MM", "en"));
-}
-
-// ---------------------
-
-$('#reservationDateTimePicker').datepicker({
-    todayHighlight: true,
-    maxViewMode: 0
-}).on('changeMonth', function(e) {
-    updateDatepickerUi(e.target, e.date); 
+$( "#datepicker" ).datepicker({
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    navigationAsDateFormat: true,
+    nextText: 'MM',
+    prevText: 'MM',
 });
 
-updateDatepickerUi('#reservationDateTimePicker');
-
-// ---------------------
 
