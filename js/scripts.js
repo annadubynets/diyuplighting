29dcd400bf524f3b97d385498edc264cb6a3b62a
Anna Dubynets <arogulina@gmail.com>
2020-06-08 22:48:11 +0300

    fix carousel section

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,22 @@ $( "#datepicker" ).datepicker({
     prevText: 'MM',
 });
 
+$('.owl-carousel').owlCarousel({
+
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 50,
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
 
