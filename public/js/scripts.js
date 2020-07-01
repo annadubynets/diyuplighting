855bd919409ec49994cd7531a485219a4bdf4c06
Anna Dubynets <arogulina@gmail.com>
2020-07-01 18:23:04 +0300

    fix

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,42 +24,78 @@ $('.testimonials-section .owl-carousel').owlCarousel({
             items:2
         }
     }
-})
+});
 
-$('.gallery-section .owl-carousel').owlCarousel({
+$('.product .owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
-    center: true,
-    navContainer: '.gallery-nav',
-    dotsContainer: '.gallery-dot',
-    responsive:{
-        0:{
-            items:1,
-            stagePadding:40,
-        },
-        600:{
-            items:2,
-            stagePadding:100,
-        },
-        1000:{
-            items:3,
-            stagePadding:100,
-        }
-    }
+    nav:false,
+    items:1,
+    dotsData: true,
+    dotsContainer:'.product-dot',
+});
+
+$( '.product .owl-dot' ).on( 'click', function() {
+    $('.product .owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
+    $( '.product .owl-dot' ).removeClass( 'active' );
+    $(this).addClass( 'active' );
 })
 
-$('.gallery-section .owl-carousel').on('changed.owl.carousel', function(event) {
-    showPageDescription();
+$('.gallery').each(function() {
+    var galleryId = $(this).attr('id');
 
+    console.log(galleryId);
+
+    $('#' + galleryId + ' .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        center: true,
+        navContainer : '#' + galleryId + ' .gallery-nav',
+        dotsContainer: '#' + galleryId + ' .gallery-dot',
+        responsive:{
+            0:{
+                items:1,
+                stagePadding:40,
+            },
+            600:{
+                items:2,
+                stagePadding:100,
+            },
+            1000:{
+                items:3,
+                stagePadding:100,
+            }
+        }
+    })
+
+    $('#' + galleryId + ' .owl-carousel').on('changed.owl.carousel', function(event) {
+        showPageDescription(galleryId);
+        console.log("current: ", $(event.relatedTarget).attr('data-description-id'))
+    });
+
+    $('#' + galleryId + ' .owl-carousel').on('refreshed.owl.carousel', function(event) {
+        showPageDescription(galleryId);
+    });
+
+
+    showPageDescription(galleryId);
 });
-function showPageDescription() {
-    var center = $('.gallery-section .owl-carousel .owl-item.active.center .item');
-    console.log(center.attr('data-description-id'));
-    $('.gallery-description').hide();
-    $('#'+center.attr('data-description-id')).show();
+
+function showPageDescription(galleryId) {
+    $("#" + galleryId + ' .owl-carousel .owl-item.active.center .item').each(function(){
+        $("#" + galleryId + ' .gallery-description').hide();
+        var center = $(this);
+        console.log(center.attr('data-description-id'));
+        $('#'+center.attr('data-description-id')).show();
+    });
 }
-showPageDescription();
+
+$(document).ready(function() {
+    $('#tab-selector').on('change', function (e) {
+        $('.gallery-section .nav-tabs li a').eq($(this).val()).tab('show');
+    });
+});
 
 
 
